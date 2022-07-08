6db1e61eef4b9b16ff27e9468fd0d19d1a88e7d6
Magno Kelly <mkfelipe.rj@gmail.com>
2022-07-08 18:54:44 -0300

    feat: implementa os destinos

--- main.c
+++ main.c
@@ -1,125 +1,197 @@
+// Algoritmo de Djikstra
+
+
+//Bibliotecas
 #include <stdio.h>
 #include <stdlib.h>
 #include <math.h>
 
+//Variáveis
+int destino, origem, vertices = 0;
+int custo, *custos = NULL;
 
+//Prototipação
+void dijkstra(int vertices,int origem,int destino,int *custos);
+void menu_mostrar(void);
+void grafo_procurar(void);
+void grafo_criar(void);
 
-void menu(void);
-void addGrafo(void);
-
-
-
-int destinos = 0;
-double custo;
-double *custos = [];
-int origem;
-int destino;
-
-int main(void) {
-  int opt = -1;
-  do{
-    menu();
-    scanf("%d", &opt);
-    if(opt == 1){
-      addGrafo();
+//Função principal
+int main(int argc, char **argv) {
+    int opt = -1;
+//Laço principal do menu
+    do {
+//Desenha o menu na tela
+        menu_mostrar();
+        scanf("%d", &opt);
+        switch (opt) {
+            case 1:
+//cria um novo grafo
+                grafo_criar();
+                break;
+            case 2:
+//procura os caminhos
+                if (vertices > 0) {
+                    grafo_procurar();
+                }
+                break;
+        }
+    } while (opt != 0);
+    printf("\nAlgoritmo de Dijkstra finalizado...\n\n");
+    system("pause");
+    return 0;
+}
+//Implementação do algoritmo de Dijkstra
+void dijkstra(int vertices,int origem,int destino,int *custos)
+{
+    int i, v, cont = 0;
+    int *ant, *tmp;
+    int *z; /* vertices para os quais se conhece o caminho minimo */
+    double min;
+    double dist[vertices]; /* vetor com os custos dos caminhos */
+/* aloca as linhas da matriz */
+    ant = (int*) calloc (vertices, sizeof(int *));
+    if (ant == NULL) {
+        system("color fc");
+        printf ("** Erro: Memoria Insuficiente **");
+        exit(-1);
     }
-    else if (opt == 2){
-      printf("Opção 2\n");
+    tmp = (int*) calloc (vertices, sizeof(int *));
+    if (tmp == NULL) {
+        system("color fc");
+        printf ("** Erro: Memoria Insuficiente **");
+        exit(-1);
     }
-    else if(opt == 3){
-      printf("sair\n");
+    z = (int *) calloc (vertices, sizeof(int *));
+    if (z == NULL) {
+        system("color fc");
+        printf ("** Erro: Memoria Insuficiente **");
+        exit(-1);
     }
-    else {
-      printf("Opção Inválida\n");
+    for (i = 0; i < vertices; i++) {
+        if (custos[(origem - 1) * vertices + i] !=- 1) {
+            ant[i] = origem - 1;
+            dist[i] = custos[(origem-1)*vertices+i];
+        }
+        else {
+            ant[i]= -1;
+            dist[i] = HUGE_VAL;
+        }
+        z[i]=0;
     }
-
-  }while(opt != 0);
-  return 0;
-
-  return 0;
+    z[origem-1] = 1;
+    dist[origem-1] = 0;
+/* Laco principal */
+    do {
+/* Encontrando o vertice que deve entrar em z */
+        min = HUGE_VAL;
+        for (i=0;i<vertices;i++){
+            if (!z[i]){
+                if (dist[i]>=0 && dist[i]<min) {
+                    min=dist[i];v=i;
+                }
+            }
+        }
+/* Calculando as distancias dos novos vizinhos de z */
+        if (min != HUGE_VAL && v != destino - 1) {
+            z[v] = 1;
+            for (i = 0; i < vertices; i++){
+                if (!z[i]) {
+                    if (custos[v*vertices+i] != -1 && dist[v]
+                                                      + custos[v*vertices+i] < dist[i]) {
+                        dist[i] = dist[v] + custos[v*vertices+i];
+                        ant[i] =v;
+    }
+                }
+            }
+        }
+    }while (v != destino - 1 && min != HUGE_VAL);
+/* Mostra o Resultado da busca */
+printf("\tDe %d para %d: \t", origem, destino);
+if (min == HUGE_VAL) {
+printf("Nao Existe\n");
+printf("\tCusto: \t- \n");
+}
+else {
+i = destino;
+i = ant[i-1];
+while (i != -1) {
+tmp[cont] = i+1;
+cont++;
+i = ant[i];
+}
+for (i = cont; i > 0 ; i--) {
+printf("%d -> ", tmp[i-1]);
+}
+printf("%d", destino);
+printf("\n\tCusto: %d\n",(int) dist[destino-1]);
+}
+}
+void grafo_criar(void){
+    do {
+        printf("\nInforme o numero de vertices (no minimo 3 ): ");
+        scanf("%d", &vertices);
+    } while (vertices < 3 );
+    if (!custos) {
+        free(custos);
+    }
+    custos = (int *) malloc(sizeof(int)*vertices*vertices);
+//Se o compilador falhou em alocar espaço na memória
+    if (custos == NULL) {
+        system("color fc");
+        printf ("** Erro: Memoria Insuficiente **");
+        exit(-1);
+    }
+    //Preenchendo a matriz com -1
+    for (int i = 0; i <= vertices * vertices; i++){
+        custos[i] = -1;
+    }
+    do {
+        system("cls");
+        printf("Entre com as Arestas:\n");
+        do {
+            printf("Origem (entre 1 e %d ou ‘0’ para sair): ", vertices);
+            scanf("%d", &origem);
+        } while (origem < 0 || origem > vertices);
+        if (origem) {
+            do {
+                printf("Destino (entre 1 e %d, menos %d): ", vertices,
+                       origem);
+                scanf("%d", &destino);
+            } while (destino < 1 || destino > vertices || destino ==
+                                                          origem);
+            do {
+                printf("Custo (positivo) do vertice %d para o vertice %d: ",
+                       origem, destino);
+                scanf("%d",&custo);
+            } while (custo < 0);
+            custos[(origem-1) * vertices + destino - 1] = custo;
+        }
+    } while (origem);
+}
+//Busca os menores caminhos entre os vértices
+void grafo_procurar(void){
+    int i, j;
+    system("cls");
+    system("color 03");
+    printf("Lista dos Menores Caminhos no Grafo Dado: \n");
+    for (i = 1; i <= vertices; i++) {
+        for (j = 1; j <= vertices; j++) {
+            dijkstra(vertices, i,j, custos);
+        }
+        printf("\n");
+    }
+    system("pause");
+    system("color 07");
+}
+//Desenha o menu na tela
+void menu_mostrar(void){
+    system("cls");
+    printf("Implementacao do Algoritmo de Dijasktra\n");
+    printf("Opcoes:\n");
+    printf("\t 1 - Adicionar um Grafo\n");
+    printf("\t 2 - Procura Os Menores Caminhos no Grafo\n");
+    printf("\t 0 - Sair do programa\n");
+    printf("? ");
 }
 
-void menu(void){
-  printf("ALGORITMO DE DIJKSTRA\n");
-  printf("Selecione as opções possíveis :\n");
-  printf("\t 1 - Add um Grafo\n");
-  printf("\t 2 - Busca os caminhos mais curtos no grafo\n");
-  printf("\t 3 - Sair\n");
-}
-
-void addGrafo(void){
-
-    printf("Favor informar o número de vértices :");
-    scanf("%d", &destinos);
-  if(destinos < 3){
-    custos = (double*) malloc(sizeof(double)*destinos*destinos);
-    if(custos == NULL){
-      printf("Memória não é suficiente");
-      exit (-1);
-    }
-    for(int i=0; i <= destinos; i++){
-      custos[i] = -1;
-    }
-    }
-
-  do{
-    system("clear");
-    printf("Informe as arestas :\n");
-    do{
-      printf("Origem (entre 1 e %d ou '0' para sair)", destinos);
-      scanf("%d", &origem);
-    } while (origem < 0 || origem > destinos);
-    if(origem){
-      do{
-        printf("Destino (entre 1 e %d, - %d)", destinos, origem);
-        scanf("%d", &destino);
-        }while(destino < 1 || destino > destinos || destino == origem);
-
-      do{
-        printf("O custo (positivo) da origem %d para o destino %d :", origem, destino);
-      }while(custo < 0);
-      custos[(origem-1)* destinos + destino -1] = custo;
-    }
-  }while(origem);
-
-
-
-}
-
-void principalFunction(int destinos, int origem, int destino, double custos){
-
-  int *x, *y, *z, i;
-  double dist[destinos];
-
-  x = (int *) calloc(destinos, sizeof(int *));
-  if(x == NULL){
-    printf("Oops: Erro de Memória");
-    exit(-1);
-  }
-  y = (int *) calloc(destinos, sizeof(int *));
-  if(x == NULL){
-    printf("Oops: Erro de Memória");
-    exit(-1);
-  }
-  z = (int *) calloc(destinos, sizeof(int *));
-  if(x == NULL){
-    printf("Oops: Erro de Memória");
-    exit(-1);
-  }
-
-  for(int i = 0; i < destinos; i++){
-    if(custos[(origem - 1)*(destinos + i)] != -1){
-      x[i] = origem -1;
-      dist[i] = custos [(origem-1) * destinos + 1];
-
-    }
-    else{
-      x[i]=-1;
-      dist[i]= HUGE_VAL;
-    }
-    z[i] = 0;
-  }
-  z[origem-1] =1;
-  dist[origem-1]=0;
-
-}
